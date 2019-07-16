8fb0f77076a49d1fdb45900d5baa8ea1cc7205c2
zejunwen <zejunwen@qq.com>
2019-07-16 11:17:33 +0800

    add CMakeLists.txt

---
+++ build/CMakeFiles/bare_earth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bare_earth.dir/bare_earth.cpp.o"
+  "bare_earth.pdb"
+  "bare_earth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bare_earth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
